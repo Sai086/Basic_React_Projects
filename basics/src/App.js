7269c2c9f2a428ef27c13f954c70be3357976ee0
Sai086 <saigudiguntla386@gmail.com>
2024-12-26 17:08:56 +0530

    Update

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,6 +1,24 @@
+import Header from "./Header";
+import Home from "./Home";
+import Contact from "./Contact";
+ import About from "./About";
+import Greeting from "./greeting";
+
 function App() {
   return (
+    <div>
     <h1>Hello from React  JS.......</h1>
+    <Header/>
+    <Home/>
+    <Contact/>
+    <About/>
+    <Greeting name={'Sai'}/>
+    <Greeting name={'Sasd'}/>
+    
+
+
+
+    </div>
   );
 }
 export default App; 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header() {
+    return (
+        <>
+        <h1>Heading 1</h1>
+        <header>Some header component</header>
+        </>
+    );
+}
+export default Header;
